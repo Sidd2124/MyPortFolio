cfae700a5e3159252cca3e928de59bc5e7cfda87
Sidd@2124 <siddhumsd@gmail.com>
2024-04-19 22:43:00 +0530

    LanguageChange Button in Contact

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,15 +1,19 @@
 
 
 import './Contact.css'
-
 import CartoonLogo from './CarttonLogo.png'
 import { Link } from "react-router-dom";
 import { IoHomeOutline } from "react-icons/io5";
 import Dail from '../Assets/Dailer.png'
 import Insta from '../Assets/Insta.png'
 import WhatsApp from '../Assets/whatsapp.png'
+import { useState } from 'react';
 
 const Contact = () => {
+  const [changeLanguage,setchangeLanguage]=useState(true)
+  const UpdateLanguage=()=>{
+    setchangeLanguage(!changeLanguage)
+  }
   return (
     <div className="ContactTop">
       <div className='ProfileDetails'>
@@ -42,8 +46,8 @@ const Contact = () => {
 
             <a href='https://www.instagram.com/mr.sidd07?igsh=MXRtNzl6ZGVvNXBxcw==' aria-label="Call me"> <img src={Insta} className="Dialer" alt="Dial" /></a>
           </div>
-
-          <div className="PerSonalInfo">
+          <button className='LangugeChangeButton' onClick={UpdateLanguage}>{changeLanguage?"తెలుగు కోసం":"For English"}</button>
+{changeLanguage? <div className="PerSonalInfo">
             <h1>Information</h1>
             <p>Name: Sidd</p>
             <p>Salary: 25000</p>
@@ -53,7 +57,20 @@ const Contact = () => {
             <p>Own Houses in Allipur & Hyderabad</p>
             <p>5 Acres Of Land Includeing </p>
             <p>{"(1.5 దేవుడి మాన్యం)"}  In Allipur</p>
-          </div>
+          </div>:
+           <div className="PerSonalInfo">
+           <h1>సమాచారం</h1>
+<p>పేరు: సిద్ధు</p>
+<p>జీతం: 25000</p>
+<p>ఉద్యోగం: ReactJS Developer</p>
+<p>Company: Agilewits Solutions</p>
+<p>అన్నదాములు&అక్కాచెల్లెలు:   ఒక్క తమ్ముడు</p>
+<p>అల్లిపుర్ మరియు హైదరాబాద్‌లో సొంత ఇల్లు ఉంది</p>
+<p>అల్లిపుర్‌లో 5 ఎకరాల భూమి ఉంది</p>
+<p>{"(1.5 ఎకరాల దేవుడి మాన్యం)"}  కలిపి</p>
+
+         </div>}
+         
           <div>
             <div>
               <h3>
